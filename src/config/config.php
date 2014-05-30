79e5d06a783b0c27595dbf6b83bbc913afc28e8d
Barry vd. Heuvel <barryvdh@gmail.com>
2014-05-30 18:40:54 +0200

    Fix default config

--- src/config/config.php
+++ src/config/config.php
@@ -16,19 +16,28 @@ return array(
      |
      */
     'defaults' => array(
-        'allow_credentials' => false,
-        'allow_origin' => array(),
-        'allow_headers' => array(),
-        'allow_methods' => array(),
-        'expose_headers' => array(),
-        'max_age' => 0,
+        'supportsCredentials' => false,
+        'allowedOrigins' => array(),
+        'allowedHeaders' => array(),
+        'allowedMethods' => array(),
+        'exposedHeaders' => array(),
+        'maxAge' => 0,
+        'hosts' => array(),
     ),
+
     'paths' => array(
-        '^/ajax/' => array(
-            'allow_origin' => array('*'),
-            'allow_headers' => array('*'),
-            'allow_methods' => array('POST', 'PUT', 'GET', 'DELETE', 'OPTIONS'),
-            'max_age' => 3600,
+        '^/api/' => array(
+            'allowedOrigins' => array('*'),
+            'allowedHeaders' => array('Content-Type'),
+            'allowedMethods' => array('POST', 'PUT', 'GET', 'DELETE'),
+            'maxAge' => 3600,
+        ),
+        '^/' => array(
+            'allowedOrigins' => array('*'),
+            'allowedHeaders' => array('Content-Type'),
+            'allowedMethods' => array('POST', 'PUT', 'GET', 'DELETE'),
+            'maxAge' => 3600,
+            'hosts' => array('^api\.'),
         ),
     ),
 
