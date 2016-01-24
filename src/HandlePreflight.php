7dc8a4b44afecca5efce7344b584618ff4079028
Barry vd. Heuvel <barryvdh@gmail.com>
2016-01-24 16:30:57 +0100

    Check global middleware also
    Addition to #92

--- src/HandlePreflight.php
+++ src/HandlePreflight.php
@@ -3,16 +3,18 @@
 use Closure;
 use Asm89\Stack\CorsService;
 use Illuminate\Routing\Router;
+use Illuminate\Contracts\Http\Kernel;
 
 class HandlePreflight
 {
 	/**
 	 * @param CorsService $cors
 	 */
-	public function __construct(CorsService $cors, Router $router)
+	public function __construct(CorsService $cors, Router $router, Kernel $kernel)
 	{
 		$this->cors = $cors;
 		$this->router = $router;
+		$this->kernel = $kernel;
 	}
 
 	/**
@@ -36,13 +38,26 @@ class HandlePreflight
 
 	/**
 	 * Verify the current OPTIONS request matches a CORS-enabled route
+	 *
 	 * @param  \Illuminate\Http\Request  $request
 	 * @return boolean
 	 */
 	private function hasMatchingCorsRoute($request)
 	{
+		// Check if CORS is added in a global middleware
+		if ($this->kernel->hasMiddleware(HandleCors::class)) {
+			return true;
+		}
+
+		// Check if CORS is added as a route middleware
 		$request = clone $request;
 		$request->setMethod($request->header('Access-Control-Request-Method'));
-		return in_array('cors', $this->router->getRoutes()->match($request)->middleware());
+
+		try {
+			$middleware = $this->router->getRoutes()->match($request)->middleware();
+			return in_array('cors', $middleware);
+		} catch (\Exception $e){
+			return false;
+		}
 	}
 }
