4f0305a9bbeb4c43ccfe9b77a1529a3a2985a93b
Barry vd. Heuvel <barryvdh@gmail.com>
2016-01-24 17:29:19 +0100

    Create HandlePreflightSimple.php

---
+++ src/HandlePreflightSimple.php
@@ -0,0 +1,34 @@
+<?php namespace Barryvdh\Cors;
+
+use Closure;
+use Asm89\Stack\CorsService;
+
+class HandlePreflightSimple
+{
+	/**
+	 * @param CorsService $cors
+	 */
+	public function __construct(CorsService $cors)
+	{
+		$this->cors = $cors;
+	}
+
+	/**
+	 * Handle an incoming OPTIONS request.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  \Closure  $next
+	 * @return mixed
+	 */
+	public function handle($request, Closure $next)
+	{
+		$response = $next($request);
+
+		if ($this->cors->isPreflightRequest($request)) {
+			$preflight = $this->cors->handlePreflightRequest($request);
+			$response->headers->add($preflight->headers->all());
+		}
+
+		return $response;
+	}
+}
