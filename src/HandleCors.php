272a6b831a10118e294dc3805a4d841a50e7cecc
Barry vd. Heuvel <barryvdh@gmail.com>
2017-03-15 14:37:52 +0100

    Add headers the normal way, used event handled as fallback

--- src/HandleCors.php
+++ src/HandleCors.php
@@ -4,7 +4,8 @@ use Closure;
 use Exception;
 use Illuminate\Contracts\Events\Dispatcher;
 use Illuminate\Foundation\Http\Events\RequestHandled;
-use Illuminate\Http\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Throwable;
 
 class HandleCors
@@ -41,17 +42,35 @@ class HandleCors
 			return new Response('Not allowed.', 403);
 		}
 
-		// Add the headers on the Request Handled event
+		// Add the headers on the Request Handled event as fallback in case of exceptions
 		if (class_exists(RequestHandled::class)) {
             $this->events->listen(RequestHandled::class, function(RequestHandled $event) use ($cors) {
-                $cors->addActualRequestHeaders($event->response, $event->request);
+                $this->addHeaders($cors, $event->request, $event->response);
             });
         } else {
-            $this->events->listen('kernel.handled', function($request, $response) use ($cors) {
-                $cors->addActualRequestHeaders($response, $request);
+            $this->events->listen('kernel.handled', function(Request $request, Response $response) use ($cors) {
+                $this->addHeaders($cors, $request, $response);
             });
         }
 
-        return $next($request);
-	}
+        $response = $next($request);
+
+        return $this->addHeaders($cors, $request, $response);
+    }
+
+    /**
+     * @param CorsService $cors
+     * @param Request $request
+     * @param Response $response
+     * @return Response
+     */
+	protected function addHeaders(CorsService $cors, Request $request, Response $response)
+    {
+        // Prevent double checking
+        if ( ! $response->headers->has('Access-Control-Allow-Origin')) {
+            $response = $cors->addActualRequestHeaders($response, $request);
+        }
+
+        return $response;
+    }
 }
