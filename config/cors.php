807ce3fd53ba3557fb1ed7c9bd5726f05d3188e3
Barry vd. Heuvel <barryvdh@gmail.com>
2019-12-23 21:11:43 +0100

    Reformat config

--- config/cors.php
+++ config/cors.php
@@ -1,32 +1,60 @@
 <?php
 
 return [
-   /*
-   |--------------------------------------------------------------------------
-   | Laravel CORS Paths
-   |--------------------------------------------------------------------------
-   |
-   | You can allow 1 or multiple paths to add CORS headers.
-   | Example: 'api/*'
-   |
-   */
-    'paths' => [],
 
     /*
     |--------------------------------------------------------------------------
     | Laravel CORS Options
     |--------------------------------------------------------------------------
     |
-    | allowed_origins, allowed_headers and allowed_methods can be set to array('*')
-    | to accept any value.
+    | The allowed_methods and allowed_headers options are case-insensitive.
+    |
+    | You don't need to provide both allowed_origins and allowed_origins_patterns.
+    | If one of the strings passed matches, it is considered a valid origin.
+    |
+    | If array('*') is provided to allowed_methods, allowed_origins or allowed_headers
+    | all methods / origins / headers are allowed.
     |
     */
-    'supports_credentials' => false,
-    'allowed_origins' => ['*'],
-    'allowed_origins_patterns' => [],
-    'allowed_headers' => ['*'],
-    'allowed_methods' => ['*'],
-    'exposed_headers' => [],
-    'max_age' => 0,
 
+    /*
+     * You can enable CORS for 1 or multiple paths.
+     * Example: ['api/*']
+     */
+    'paths' => [],
+
+    /*
+    * Matches the request method. `[*]` allows all methods.
+    */
+    'allowed_methods' => ['*'],
+
+    /*
+     * Matches the request origin. `[*]` allows all origins.
+     */
+    'allowed_origins' => ['*'],
+
+    /**
+     * Matches the request origin with, similar to `Request::is()`
+     */
+    'allowed_origins_patterns' => [],
+
+    /**
+     * Sets the Access-Control-Allow-Headers response header. `[*]` allows all headers.
+     */
+    'allowed_headers' => ['*'],
+
+    /**
+     * Sets the Access-Control-Expose-Headers response header.
+     */
+    'exposed_headers' => false,
+
+    /**
+     * Sets the Access-Control-Max-Age response header.
+     */
+    'max_age' => false,
+
+    /**
+     * Sets the Access-Control-Allow-Credentials header.
+     */
+    'supports_credentials' => false,
 ];
