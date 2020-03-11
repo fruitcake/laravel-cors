6d46adb112cd844851b81968b8b55b3d12abc43a
Barry vd. Heuvel <barry@fruitcake.nl>
2020-03-11 08:19:37 +0100

    Update config defaults

--- config/cors.php
+++ config/cors.php
@@ -44,14 +44,14 @@ return [
     'allowed_headers' => ['*'],
 
     /*
-     * Sets the Access-Control-Expose-Headers response header.
+     * Sets the Access-Control-Expose-Headers response header with these headers.
      */
-    'exposed_headers' => false,
+    'exposed_headers' => [],
 
     /*
-     * Sets the Access-Control-Max-Age response header.
+     * Sets the Access-Control-Max-Age response header when > 0.
      */
-    'max_age' => false,
+    'max_age' => 0,
 
     /*
      * Sets the Access-Control-Allow-Credentials header.
