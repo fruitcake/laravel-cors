092a0f86334663518a1c2e8cb22c977d480d1d3b
Barry vd. Heuvel <barry@fruitcake.nl>
2022-02-20 19:23:58 +0100

    Mark method as deprecated

--- src/HandleCors.php
+++ src/HandleCors.php
@@ -79,6 +79,7 @@ class HandleCors
      * Add the headers to the Response, if they don't exist yet.
      *
      * @param RequestHandled $event
+     * @deprecated
      */
     public function onRequestHandled(RequestHandled $event)
     {
